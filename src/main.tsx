9485193b1f8eedd46bf8fe5393d6ec590cab4222
felistus <felistusobieze@gmail.com>
2022-05-27 22:07:33 +0100

    added react-router created attendance-form

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import AttendanceForm from "./routes/attendance-form";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="attendance-form" element={<AttendanceForm />} />
+        <Route
+          path="*"
+          element={
+            <main className="p-4">
+              <p>There is nothing Here...!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
--- src/routes/attendance-form.tsx
+++ src/routes/attendance-form.tsx
@@ -1,46 +1,38 @@
-import UserIcons from "./components/images/UserIcons";
-import WelcomeLoginImage from "./components/images/WelcomeLoginImage";
+import AttendancePenIcon from "../components/images/AttendancePenIcon";
+import SignAttendanceImage from "../components/images/SignAttendanceImage";
 
-function App() {
+export default function AttendanceForm() {
   return (
     <main className="w-full h-screen bg-slate-200">
-      <section className="w-full h-full items-center justify-center flex md:flex-row flex-col-reverse px-4 ">
+      <section className="w-full h-full items-center justify-center flex md:flex-row-reverse flex-col-reverse px-4 ">
         <div className="md:w-3/6 flex justify-center items-center  ">
           <form
             action=""
             className="md:w-[400px] max-w-[400px] bg-[#536DFE] py-4 px-8 rounded-lg  "
           >
             <div className="flex justify-center items-center ">
-              <UserIcons />
+              <AttendancePenIcon />
             </div>
             <p className="text-center text-white mt-3 text-xl font-bold uppercase">
-              Welcome!
+              mark attendance
             </p>
             <input
               type="tel"
-              name="phone-number"
-              id="phone-number"
+              name="attendance-phone-number"
+              id="attendance-phone-number"
               className="px-2 py-4 outline-none rounded-md my-4 w-full "
               placeholder="Enter your phone number!"
             />
-            <input
-              type="password"
-              id="password"
-              className="px-2 py-4 outline-none rounded-md w-full "
-              placeholder="Enter password to continue"
-            />
             <button className="w-full p-2 my-4 capitalize rounded-md bg-white hover:bg-[#CCCCCC] text-[#536DFE] font-bold">
-              login
+              mark attendance
             </button>
           </form>
         </div>
 
         <div className="md:w-3/6 p-4 mb-4  ">
-          <WelcomeLoginImage />
+          <SignAttendanceImage />
         </div>
       </section>
     </main>
   );
 }
-
-export default App;
