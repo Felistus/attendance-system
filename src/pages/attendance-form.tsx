4ad11008590e959aa7132a9e632f755812c117e5
felistus <felistusobieze@gmail.com>
2022-06-06 14:59:13 +0100

    started implementing attendance function

--- src/pages/attendance-form.tsx
+++ src/pages/attendance-form.tsx
@@ -1,13 +1,28 @@
+import { ChangeEvent, FormEvent, useContext, useRef, useState } from "react";
 import AttendancePenIcon from "../components/icons/AttendancePenIcon";
 import SignAttendanceImage from "../components/SignAttendanceImage";
+import { UserContext, UserDetailsContext } from "../context/context-file";
 
 export default function AttendanceForm() {
+  const loggedUser = useContext(UserContext).user;
+  const user = useContext(UserDetailsContext).userDetails;
+  const selectOptionRef = useRef<HTMLSelectElement>(null);
+  const [userNumber, setUserNumber] = useState<string>();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(userNumber, selectOptionRef.current?.value);
+  };
+
+  const handleUserNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserNumber(event.target.value);
+  };
   return (
     <main className="w-full h-screen bg-slate-200">
       <section className="w-full h-full items-center justify-center flex md:flex-row-reverse flex-col-reverse px-4 ">
         <div className="md:w-3/6 flex justify-center items-center  ">
           <form
-            action=""
+            onSubmit={handleSubmit}
             className="md:w-[400px] max-w-[400px] bg-[#536DFE] py-4 px-8 rounded-lg  "
           >
             <div className="flex justify-center items-center ">
@@ -18,13 +33,14 @@ export default function AttendanceForm() {
             </p>
             <input
               type="tel"
-              name="attendance-phone-number"
-              id="attendance-phone-number"
+              value={userNumber}
+              onChange={handleUserNumberChange}
               className="px-2 py-4 outline-none rounded-md my-4 w-full uppercase "
               placeholder="phone number... (eg: 08012345678)"
             />
             <select
               name="courses"
+              ref={selectOptionRef}
               id="courses"
               className="outline-none w-full rounded-md px-2 py-2 uppercase"
             >
